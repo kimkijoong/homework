84bed97c60843cecda6ceb96947ba55ac9b83291
kimkijoong <kkj6943@gmail.com>
2016-10-25 16:21:16 +0900

    return

---
+++ FDS-class/161025/js/morning.js
@@ -0,0 +1,32 @@
+// // var scope_variable = '전역 변수';
+// // console.log('전역 scope_variable:', scope_variable);
+// //
+// // function createLocalScope() {
+// //   var scope_variable = '함수 내부 지역 변수';
+// //   console.log('함수 내부 scope_variable:', scope_variable);
+// // }
+// //
+// // createLocalScope();
+// // console.log('함수 외부 scope_variable:', scope_variable);
+// function fn() {
+//   var course; //undfined
+//   if (course // undifined이기 때문에 실행 되지 않는다.) {
+//     course = 'develop';
+//     console.log(course); //
+//   }
+//   console.log(course); //develop
+// }
+// var course;
+// course = 'design';
+//
+// fn();
+//
+//
+// console.log(course); //design
+
+function sum(num1, num2) {
+  if ( typeof num1 !== 'number' || typeof num2 !== 'number') {
+    console.error('전달된 인자 값은 숫자 유형이 아닙니다. 숫자 유형을 넣어주세요.');
+  }
+  return num1 + num2;
+}
