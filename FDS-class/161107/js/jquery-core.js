34704fbbf46d28ba586e0f4ba3a958753d7562fd
kimkijoong <kkj6943@gmail.com>
2016-11-08 11:45:34 +0900

    update

---
+++ FDS-class/161107/js/jquery-core.js
@@ -0,0 +1,5 @@
+(function($){
+  'use strict';
+  var $body = $('body').attr('data-end-body', 'yes');
+  console.log('$body:', $body);
+})(jQuery);
---
+++ FDS-class/161107/js/jquery-excute-in-head.js
@@ -0,0 +1,8 @@
+jQuery(function($){
+  'use strict';
+
+  var $body;
+  $body = $(document.body);
+  $body = $('body');
+  $body.attr('data-in=-head', 'yes');
+});
