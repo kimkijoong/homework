f12499fd2dc96f3e497bad895d7d3f5489beeaa6
kimkijoong <kkj6943@gmail.com>
2016-11-09 20:45:19 +0900

    jQuery study (css etc)

--- FDS-class/161107/js/jquery-core.js
+++ FDS-class/161107/js/jquery-core.js
@@ -1,5 +1,14 @@
-(function($){
+(function(global, $){
   'use strict';
-  var $body = $('body').attr('data-end-body', 'yes');
-  console.log('$body:', $body);
-})(jQuery);
+
+  $brand.on('click',function(){
+
+    var props = $brand.css(['font-size','width','word-spacing']);
+    var html_string = ['<h3>brand properties</h3>'];
+
+    for (var prop in props) {
+      html_string.push(`<p><code>${prop}: ${props[prop]}</code></p>`);
+    }
+  })
+
+})(this, this.jQuery);
